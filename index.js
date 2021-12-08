61d99cfda1c3005ec28d078768f8b199a67ed54e
Sergio Júnior <sergio.smmjr@gmail.com>
2021-12-08 06:18:38 -0300

    Organizing code and importing Account.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-import {Client} from "./Client.js"
-import {CheckingAccount} from "./CheckingAccount.js"
+import {Client} from "./Client.js";
+import {CheckingAccount} from "./CheckingAccount.js";
+import { SavingsAccount } from "./SavingsAccount.js";
+import { Account } from "./Account.js";
 
 const client1 = new Client("Ricardo", 11122233309);
 const client2 = new Client("Alice", 88822233309);
 
-const account1 = new CheckingAccount(1001, client1);
+const account1 = new Account(1001, client1);
+const account2 = new Account(102, client2);
+
+const savingsAccount1 = new Account(50, client1, 1001);
+
 account1.deposit(500);
-const account2 = new CheckingAccount(102, client2);
+account1.withdraw(100);
 
-let value = 200;
-account1.transfer(value, account2);
-
+console.log(savingsAccount1)
 console.log(account1);
-console.log(account2);
-console.log(CheckingAccount.accountCounter)
+
 
 
 
