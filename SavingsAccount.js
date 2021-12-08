e2d3d52c8dddcf121c9ce417ce9c668fd347fcfe
Sergio Júnior <sergio.smmjr@gmail.com>
2021-12-08 06:17:10 -0300

    Creating SavingsAccount

---
+++ SavingsAccount.js
@@ -0,0 +1,26 @@
+export class SavingsAccount{
+    constructor(openingBalance, client, agency){
+        this._balance = openingBalance;
+        this._client = client;
+        this._agency = agency;
+    }
+
+    withdraw(value) {
+        if (this._balance >= value) {
+            this._balance -= value;
+            return value;
+        }
+    }
+
+    deposit(value) {
+        if (value <= 1000) {
+            return;
+        }
+        this._balance += value;
+    }
+
+    transfer(value, account){
+        const withdrawnAmount = this.withdraw(value);
+        account.deposit(withdrawnAmount);
+    }
+}
